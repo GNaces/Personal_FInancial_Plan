1e3d679a2b39dd76c0c8522ff3b428194b3c1b47
Godwin C. Naces <godwinnaces@gmail.com>
2024-06-19 14:40:06 +0000

    Import gsread to access the spreadsheet, import credentials for authentication, and set the IAM which specifies the scope

--- run.py
+++ run.py
@@ -1,3 +1,13 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# In order to access and change data on a spreadsheet, import the gspread library.
+import gspread
+
+# For authentication, import the Credentials class.
+from google.oauth2.service_account import Credentials
+
+# Scope constant for IAM.
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
