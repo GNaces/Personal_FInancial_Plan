970fb928481d02f91ca82743cc7b2876c5a4de8f
Godwin Naces <godwinnaces@gmail.com>
2024-06-20 16:41:05 +0000

    resolve error by changing the filename to Personal_Financial_Plan

--- run.py
+++ run.py
@@ -143,7 +143,7 @@ def main():
     tracker = FinancialTracker(sheet)
 
     # This line calls the load_data_from_file method of the tracker object to load any existing budget data from a file named Personal Financial Plan
-    tracker.load_data_from_file("Personal Financial worksheet")
+    tracker.load_data_from_file("Personal_Financial_Plan")
 
     # Set the monthly income
     while True:
@@ -172,7 +172,7 @@ def main():
     tracker.display_summary()
 
     # Save data to a file
-    tracker.save_data_to_file("Personal Financial worksheet")
+    tracker.save_data_to_file("Personal_Financial_Plan")
 
 if __name__ == "__main__":
     main()
